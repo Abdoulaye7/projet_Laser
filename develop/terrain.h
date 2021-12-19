103ba883a2ef6796c8bb8925ee24b0fd34140cc7
Abdoulaye7 <50219253+Abdoulaye7@users.noreply.github.com>
2021-12-19 14:18:05 +0100

    Update terrain.h

--- develop/terrain.h
+++ develop/terrain.h
@@ -1,1 +1,34 @@
 
+#ifndef TERRAIN_H
+#define TERRAIN_H
+
+class Terrain{
+
+
+public:
+    Terrain(const Position&,const Position&,const vector<Mur>&);
+    Terrain(const Position&,const Position&);
+/***
+Les murs
+le cible
+le laser
+*/
+void ajouterMur(const Mur&);
+void ajouterMirroir(const Mirroir&);
+
+void changerPosiLaser(const Position&);
+void ajouterCible(const Position&);
+
+//la fonction renvoie vrai si on a tirrer sur une cible, sinon false
+bool tirrer();//principale
+void placerMur(const Mur&); //faire les tests pour que la taille du mur ne depasse pas la taille du terrain
+
+private:
+    vector<Mur> _murs;
+    vector<Mirroir> _mirroirs;
+    Position _laser;
+    Position _cible;
+
+
+};
+#endif // TERRAIN_H
