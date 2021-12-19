0eda16799921f44e11be8ee1a1ed8f33ed4a1865
Abdoulaye7 <50219253+Abdoulaye7@users.noreply.github.com>
2021-12-19 14:08:27 +0100

    Update mur.h

--- develop/mur.h
+++ develop/mur.h
@@ -1,1 +1,31 @@
 
+#ifndef MUR_H
+#define MUR_H
+
+enum Direction{NORD,SUD,EST,OUEST};
+class Mur{
+
+public:
+    Mur(const Position& , const Direction&,unsigned int);
+    Mur(const Position& , const Direction&);
+
+    Position depart()const;
+    Direction direction()const;
+    unsigned int taille()const;
+
+
+
+    void depart(const Position&);
+    void direction(const Direction&);
+    void taille(unsigned int);
+
+private:
+
+    Position d_depart;
+    Direction d_direction;
+    unsigned int d_taille;
+
+
+
+};
+#endif // MUR_H
