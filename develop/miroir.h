74407d6adff0f7f5018a03eeb97de428bcd0cde3
Abdoulaye7 <50219253+Abdoulaye7@users.noreply.github.com>
2021-12-19 14:15:06 +0100

    Update miroir.h

--- develop/miroir.h
+++ develop/miroir.h
@@ -1,1 +1,14 @@
 
+#ifndef MIROIR_H
+#define MIROIR_H
+
+
+enum MirroirType{AIGU,GRAVE};
+public:
+    Mirroir(const Position&,const MirroirType&);
+private:
+    
+    Position d_position;
+    MirroirType d_type;
+};
+#endif // MIROIR_H
