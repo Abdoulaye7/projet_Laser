0e56f893229263eb06ad4d6577b5067eb1d24e44
Abdoulaye7 <50219253+Abdoulaye7@users.noreply.github.com>
2021-12-19 14:02:34 +0100

    Update position.h

--- develop/position.h
+++ develop/position.h
@@ -1,1 +1,21 @@
 
+#ifndef POSITION_H
+#define POSITION_H
+
+class Position{
+
+public:
+    Position();
+    Position(unsigned int);
+    Position(unsigned int , unsigned int);
+
+    unsigned int x()const;
+    unsigned int y()const;
+
+    void setX(unsigned int);
+    void setY(unsigned int);
+
+private:
+    unsigned int d_x,d_y;
+};
+#endif // POSITION_H
