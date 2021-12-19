2a0b4662a5a6a08f6a9234c2eef6ee9eb4ccf312
Abdoulaye7 <50219253+Abdoulaye7@users.noreply.github.com>
2021-12-19 14:01:58 +0100

    Update position.cpp

--- develop/position.cpp
+++ develop/position.cpp
@@ -1,1 +1,32 @@
 
+#include "position.h"
+#include <iostream>
+
+
+Position::Position():Position{0,0}{}
+Position::Position(unsigned int xy):Position{xy,xy}{}
+Position::Position(unsigned int x, unsigned int y):
+    d_x{x},
+    d_y{y}
+    {
+
+    }
+
+
+unsigned int Position::x()const
+{
+    return d_x;
+}
+unsigned int Position::y()const
+{
+    return d_y;
+}
+
+void Position::setX(unsigned int x)
+{
+    d_x=x;
+}
+void Position::setY(unsigned int y)
+{
+    d_y=y;
+}
